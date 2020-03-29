877bbd2f58341188d0e021886d3478118853af61
Igor Bulakh <igorbulakh1987@yandex.ru>
2020-03-29 22:53:00 +0300

    Remake the whole project, but it is still not working

--- 4.7.7/Task 4.7.7.md
+++ 4.7.7/Task 4.7.7.md
@@ -18,57 +18,18 @@ $\displaystyle \frac{\displaystyle \sum_{j = 1}^k v_{i_j}}{\displaystyle \sum_{j
 9.33333
 ```
 
-$\displaystyle \frac{10^7}{1}$
+**Input**:
+```
+6 2
+6792 19949
+22986 3872
+28903 6506
+2514 1990
+23158 5029
+2101 23700
+```
+**Output**:
 
-$0.55555$
+```
 
-В математике `биномиальные коэффициенты` — это коэффициенты в разложении  $(1+x)^n$ по степеням `x`. Коэффициент при $x^k$ обозначается $\textstyle\binom{n}{k}$ или $\textstyle C_n^k$ и читается «биномиальный коэффициент из `n` по `k`» (или «число сочетаний из `n` по `k`», $\textstyle C_n^k$ читается как «це из `n` по `k`»):
-{{EF|:|$(1+x)^n=\binom{n}{0}+\binom{n}{1}x+\binom{n}{2}x^2+\ldots+\binom{n}{n}x^n =\sum_{k=0}^{n} \binom{n}{k} x^k,$|ref=1}}
-для [[натуральное число|натуральных]] степеней $n$ 
-
-$$\frac{\sum v_i}{\sum w_i} - \frac{\sum v_j}{\sum w_j} > 0$$
-
-$$
-V_{big} := \sum v_i := V + v_a\
-W_{big} := \sum w_i := W + w_a\
-$$
-$$
-V_{small} := \sum v_j := V + v_b\
-W_{small} := \sum w_j := W + w_b\
-$$
-
-$$\frac{\sum v_i}{\sum w_i} - \frac{\sum v_j}{\sum w_j} > 0$$  multiply by $${\sum w_i} \cdot {\sum w_j}$$
-
-$$\sum v_i \cdot \sum w_j - \sum v_j \cdot \sum w_i > 0$$
-
-$$(V+v_i)(W+w_j) - (V + v_j)(W + w_i) > 0$$
-
-$$V W + V w_j + v_i W + v_i w_j - V W - V w_i - v_j W - v_j w_i > 0$$
-
-$$V(w_j - w_i) + W(v_i - v_j) + v_i w_j - v_j w_i > 0$$
-
-$$v_i w_j = v_i (w_j - w_i + w_i) = v_i (w_j - w_i) + v_i w_i$$
-
-$$V(w_j - w_i) + W(v_i - v_j) + v_i (w_j - w_i) + v_i w_i - v_j w_i > 0$$
-
-$$(w_j - w_i)(V + v_i) + W(v_i - v_j) + w_i(v_i - v_j) > 0$$
-
-$$(w_j - w_i)(V + v_i) + (v_i - v_j)(W + w_i) > 0$$
-
-$$(w_j - w_i)V_{big} + (v_i - v_j)W_{big} > 0$$
-
-
-$$
-\sin(a + b ) = \sin(a)\cos(b) + \cos(a)\sin(b)
-$$
-
-
-
-Depending on the value of $x$ the equation `\( f(x) = \sum_{i=0}^{n} \frac{a_i}{1+x} \)` may diverge or converge.
-
-$$ f(x) = \sum_{i=0}^{n} \frac{a_i}{1+x} $$
-
-In-line maths elements can be set with a different style: `\(f(x) = \displaystyle \frac{1}{1+x}\)`. The same is true the other way around:
-
-$$\displaystyle C_n^k$$
-$$\textstyle\binom{n}{k}$$
+```
--- 4.7.7/src/diamonds.cpp
+++ 4.7.7/src/diamonds.cpp
@@ -3,73 +3,148 @@
 #include <iomanip>
 #include <utility>
 #include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <string>
 
 
-/*
- *
-std::vector<std::string> CreateFilePath(const std::vector<int>& test_numbers, bool is_result = false) {
-    std::vector<std::string> files;
-    files.reserve(test_numbers.size());
-    for (size_t i = 0; i < test_numbers.size(); ++i) {
-        std::string temp = "../test/Tests/";
-        temp += is_result == true ? "OUT " : "IN ";
-        temp += std::to_string(test_numbers[i]) + ".txt";
-        files.push_back(temp);
+double CheckData(std::vector<std::pair<int, int>>& val, int k) {
+    long long V_big = 0;
+    long long W_big = 1;
+
+    // Считаем суммы ценности и веса
+    for (int i = 0; i < k; ++i) {
+        V_big += val[i].first;
+        W_big += val[i].second;
     }
-    return files;
-}
-void Test(std::vector<std::pair<int, int>> val, int k, std::ostream& os = std::cout) {
-    double res = CountBest(val, k);
-    // std::cout << res << std::endl;
-    std::cout << std::fixed << std::setprecision(5) << res << std::endl;
-    os << std::fixed << std::setprecision(5) << BinarySearch(val, res, k);
-}
-void RunTests(const std::vector<int>& nums) {
-    int n = 0;
-    int k = 0;
-    int v, w;
-    double res = 0;
-    std::vector<std::pair<int, int>> val;
-    std::vector<std::string> files = CreateFilePath(nums);
-    std::vector<std::string> answers = CreateFilePath(nums, true);
 
-    for (size_t i = 0; i < files.size(); ++i) {
-        val.clear();
-        std::ifstream test(files[i]);
-        std::ifstream answer(answers[i]);
-        if (test.is_open()) {
-            test >> n >> k;
-            val.reserve(n);
-            while (test >> v >> w) {
-                val.push_back(std::make_pair(v, w));
+    // Удельное значение стоимости
+    double result_origin = static_cast<double>(V_big) / static_cast<double>(W_big);
+
+    // Создаем переменные для сумм V_j и W_j
+    long long V_j = 0;
+    long long W_j = 1;
+
+    // Контейнер для 
+    std::vector<double> compare(k);
+
+    // Максимальное значение для удельной стоимости
+    double MAX = result_origin;
+
+    int index = -1;
+    for (size_t j = k; j < val.size(); ++j) {
+        // Устанавлиеваем начальное значение для сумм равное текущей драгоценности
+        V_j = val[j].first;
+        W_j = val[j].second;
+
+        // Подставляем значения поочередно вместо каждого элемента K последовательности
+        // Считаем удельную стоимость и сохраняем в вектор
+        for (int m = k - 1; m >= 0; --m) {
+            for (int i = 0; i < k; ++i) {
+                if (i == m) {
+                    continue;
+                } else {
+                    V_j += val[i].first;
+                    W_j += val[i].second;
+                }
+            }
+            compare[m] = static_cast<double>(V_j) / static_cast<double>(W_j);
+        }
+
+        // Сравниваем каждое полученное значение удельной стоимости 
+        // с нашим текущим значением и сохраняем максимум
+        for (size_t i = 0; i < compare.size(); ++i) {
+            if (compare[i] > MAX) {
+                MAX = compare[i];
+                index = i;
             }
         }
-        test.close();
-        if (answer.is_open()) {
-            answer >> res;
+
+        // Проверяем если максимум изменился, то мы заменяем максимальное значение на соответсвующей позиции
+        if (MAX > result_origin) {
+            val[index] = std::make_pair(val[j].first, val[j].second);
         }
-        answer.close();
-        std::ostringstream oss;
-        std::ostringstream rs;
-        Test(val, k, oss);
-        rs << std::fixed << std::setprecision(5) << res;
-        if (oss && oss.str() == rs.str()) {
-            std::cout << "TEST #" << nums[i] << " is OK" << std::endl;
-        } else {
-            std::cout << "TEST #" << nums[i] << " is FAILED" << std::endl;
-            std::cout << "Expected: " << rs.str() << ", got: " << oss.str() << std::endl;
-            std::cout << std::endl;
+        
+        // Очищаем контейнера и обнуляем переменные
+        compare.clear();
+        compare.resize(k);
+        index = -1;
+        V_big = 0;
+        W_big = 0;
+
+        // Просчитываем суммы с учетом новых данных
+        for (int i = 0; i < k; ++i) {
+            V_big += val[i].first;
+            W_big += val[i].second;
         }
     }
-
+    return static_cast<double>(V_big) / static_cast<double>(W_big);
 }
-*
-*/
+
+
+// std::vector<std::string> CreateFilePath(const std::vector<int>& test_numbers, bool is_result = false) {
+//     std::vector<std::string> files;
+//     files.reserve(test_numbers.size());
+//     for (size_t i = 0; i < test_numbers.size(); ++i) {
+//         std::string temp = "../test/Tests/";
+//         temp += is_result == true ? "OUT " : "IN ";
+//         temp += std::to_string(test_numbers[i]) + ".txt";
+//         files.push_back(temp);
+//     }
+//     return files;
+// }
+
+
+// void Test(std::vector<std::pair<int, int>> val, int k, std::ostream& os = std::cout) {
+//     double res = CheckData(val, k);
+//     os << std::fixed << std::setprecision(5) << res;
+// }
+
+
+// void RunTests(const std::vector<int>& nums) {
+//     int n = 0;
+//     int k = 0;
+//     int v, w;
+//     double res = 0;
+//     std::vector<std::pair<int, int>> val;
+//     std::vector<std::string> files = CreateFilePath(nums);
+//     std::vector<std::string> answers = CreateFilePath(nums, true);
+//     for (size_t i = 0; i < files.size(); ++i) {
+//         val.clear();
+//         std::ifstream test(files[i]);
+//         std::ifstream answer(answers[i]);
+//         if (test.is_open()) {
+//             test >> n >> k;
+//             val.reserve(n);
+//             while (test >> v >> w) {
+//                 val.push_back(std::make_pair(v, w));
+//             }
+//         }
+//         test.close();
+//         if (answer.is_open()) {
+//             answer >> res;
+//         }
+//         answer.close();
+
+//         std::ostringstream oss;
+//         std::ostringstream rs;
+//         Test(val, k, oss);
+
+//         rs << std::fixed << std::setprecision(5) << res;
+//         if (oss && oss.str() == rs.str()) {
+//             std::cout << "TEST #" << nums[i] << " is OK" << std::endl;
+//         } else {
+//             std::cout << "TEST #" << nums[i] << " is FAILED" << std::endl;
+//             std::cout << "Expected: " << rs.str() << ", got: " << oss.str() << std::endl;
+//             std::cout << std::endl;
+//         }
+//     }
+// }
+
 
 
 int main() {
     int n, k;
-    double res = 0.0;
     std::cin >> n >> k;
     int v = 0;
     int w = 0;
@@ -80,7 +155,20 @@ int main() {
         val.push_back(std::make_pair(v, w));
     }
 
-    
+    double res = CheckData(val, k);
+    std::cout << std::fixed << std::setprecision(5) << res << std::endl;
+
+    // std::vector<std::pair<int, int>> values = {{6792, 19949}, {22986, 3872},
+    //     {28903, 6506}, {2514, 1990}, {23158, 5029}, {2101, 23700}};
+
+    // std::vector<std::pair<int, int>> values = {{10, 1}, {10, 1},
+    //     {9000, 1000}, {8, 1}};
+
+    // std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    //                 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+
+    // RunTests(nums);
+
 
     return 0;
 }
